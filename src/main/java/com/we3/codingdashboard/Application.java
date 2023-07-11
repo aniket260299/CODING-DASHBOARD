cc8b622ddf983fc2f01843d32c1982bb693b9d29
aniket2602199 <aniket.kumar@outlook.in>
2023-07-11 22:53:06 +0530

    Initial commit

---
+++ src/main/java/com/we3/codingdashboard/Application.java
@@ -0,0 +1,13 @@
+package com.we3.codingdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
