736a3a87d3f60d7bc5d4b12894da24b6e22ebd67
aniket2602199 <aniket.kumar@outlook.in>
2023-07-12 02:32:12 +0530

    Code Format

--- src/main/java/com/we3/codingdashboard/model/CodingDashboard.java
+++ src/main/java/com/we3/codingdashboard/model/CodingDashboard.java
@@ -4,16 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
-public record CodingDashboard(
-                @Id Integer id,
-                String title,
-                String solution,
-                String hint,
-                String revision_notes,
-                String link,
-                Integer difficulty,
-                String tags,
-                LocalDateTime date_created,
-                LocalDateTime date_updated) {
-
+public record CodingDashboard(@Id Integer id, String title, String solution, String hint, String revision_notes,
+                              String link, Integer difficulty, String tags, LocalDateTime date_created,
+                              LocalDateTime date_updated) {
 }
--- src/main/java/com/we3/codingdashboard/repository/CodingDashboardRepository.java
+++ src/main/java/com/we3/codingdashboard/repository/CodingDashboardRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.ListCrudRepository;
 import com.we3.codingdashboard.model.CodingDashboard;
 
 public interface CodingDashboardRepository extends ListCrudRepository<CodingDashboard, Integer> {
-
 }
