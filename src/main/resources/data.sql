ba8ae25bdb6b8109568f8b013bcb2fb7918577b8
aniket260299 <aniket.kumar@outlook.in>
2023-10-16 04:59:49 +0530

    added test user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,6 @@ INSERT INTO coding_dashboard (title, solution, hint, notes, link, difficulty, ta
 INSERT INTO coding_dashboard (title, solution, hint, notes, link, difficulty, tags, date_created, date_updated, username) VALUES('title6', 'solution6', 'hint6', 'revisionNotes6', 'link6', 3, 'test', '2013-07-09 03:23:34.234', '2013-07-09 03:23:34.234', 'test');
 
 INSERT into users values ('admin', '$2a$10$bYWv9phxNpY52pj3BXOun..4T23ZemgtC7xJAlnJ8lY9h2IYVWWL2', TRUE);
+INSERT into users values ('test', '$2a$10$bYWv9phxNpY52pj3BXOun..4T23ZemgtC7xJAlnJ8lY9h2IYVWWL2', TRUE);
 INSERT into authorities values ('admin', 'ROLE_ADMIN');
-INSERT into authorities values ('admin', 'ROLE_USER');
+INSERT into authorities values ('test', 'ROLE_USER');
